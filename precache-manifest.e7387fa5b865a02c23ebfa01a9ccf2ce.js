1c4c575ef2963c79dea85f76398575cca7a68979
paulwebdevgit <paulkonev94@gmail.com>
2021-05-21 22:42:04 +0200

    Updates

---
+++ precache-manifest.e7387fa5b865a02c23ebfa01a9ccf2ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8bbcf09e8cb66345fba8541bdc245cb",
+    "url": "/spacex_react/index.html"
+  },
+  {
+    "revision": "f05ce73e84c833bc4384",
+    "url": "/spacex_react/static/css/main.920540b6.chunk.css"
+  },
+  {
+    "revision": "638e9e0054630750441a",
+    "url": "/spacex_react/static/js/2.1eaa6711.chunk.js"
+  },
+  {
+    "revision": "88c0bfd11a2be5419414edbddb842b0f",
+    "url": "/spacex_react/static/js/2.1eaa6711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f05ce73e84c833bc4384",
+    "url": "/spacex_react/static/js/main.f817e651.chunk.js"
+  },
+  {
+    "revision": "1d74122af92d530b2658",
+    "url": "/spacex_react/static/js/runtime-main.86c0c38a.js"
+  }
+]);
